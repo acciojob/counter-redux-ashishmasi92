f991aa661b1d90cc53aab3c6d756ee819fade547
ashish <ashishmasih92g@gmail.com>
2025-07-29 18:51:39 +0530

    assignment

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -4,8 +4,8 @@ import { increment, decrement, reset } from "../Redux/action/actionCreator.js";
 
 const CounterApp = () => {
 
-    let x = useSelector(x => x)
-    console.log(x);
+    let {count} = useSelector(x => x)
+    console.log(count);
 
     let dispatch = useDispatch()
 
@@ -13,14 +13,14 @@ const CounterApp = () => {
     return (
         <>
 
-            <h1 data-testid="counter-value" >{x.count}</h1>
+            <div data-testid="counter-value">{count}</div>
             <div>
                 <button data-testid="increment-btn" onClick={() => {
                     dispatch(increment())
-                }} >increment</button>
+                }} >+</button>
                 <button data-testid="decrement-btn" onClick={() => {
                     dispatch(decrement())
-                }} >decrement</button>
+                }} >-</button>
 
             </div>
 
